05b5f42fb3a52049f5d83dcfa335a7b9a38fde7f
Aaron Hunter <aa_hunter@yahoo.com>
2025-04-24 11:38:06 -0700

    UDP_server updated to accept command data

--- UDP_server.c
+++ UDP_server.c
@@ -5,8 +5,15 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define BUF_SIZE 500
+#define CMD_SIZE 52 // 52 bytes for 26 int16_t values
+
+union CMD_DATA {
+    unsigned char bytes[CMD_SIZE];
+    int16_t values[CMD_SIZE/2];
+} cmd_data, buf_data;
 
 int
 main(int argc, char *argv[])
@@ -68,7 +75,7 @@ main(int argc, char *argv[])
         char host[NI_MAXHOST], service[NI_MAXSERV];
 
         peer_addrlen = sizeof(peer_addr);
-        nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+        nread = recvfrom(sfd, cmd_data.bytes, CMD_SIZE, 0,
                          (struct sockaddr *) &peer_addr, &peer_addrlen);
         if (nread == -1)
             continue;               /* Ignore failed request */
@@ -76,13 +83,19 @@ main(int argc, char *argv[])
         s = getnameinfo((struct sockaddr *) &peer_addr,
                         peer_addrlen, host, NI_MAXHOST,
                         service, NI_MAXSERV, NI_NUMERICSERV);
-        if (s == 0)
+        if (s == 0) {
             printf("Received %zd bytes from %s:%s\n",
                    nread, host, service);
+            for (size_t i = 0; i < nread/2; i++) {
+                buf_data.values[i] = cmd_data.values[i];
+                cmd_data.values[i] = ntohs(cmd_data.values[i]);
+                printf("Received value %zu: %d\n", i, cmd_data.values[i]);
+            }
+        }
         else
             fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
 
-        if (sendto(sfd, buf, nread, 0, (struct sockaddr *) &peer_addr,
+        if (sendto(sfd, buf_data.bytes, nread, 0, (struct sockaddr *) &peer_addr,
                    peer_addrlen) != nread)
         {
             fprintf(stderr, "Error sending response\n");
