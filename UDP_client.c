4f67f275c39b852774be6257c72878176f1f689e
Aaron Hunter <aa_hunter@yahoo.com>
2025-04-24 11:43:26 -0700

    updated UDP_client.c to send command values

--- UDP_client.c
+++ UDP_client.c
@@ -5,21 +5,29 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define BUF_SIZE 500
+#define CMD_SIZE 52 // 52 bytes for 26 int16_t values
+
+union CMD_DATA {
+    unsigned char bytes[CMD_SIZE];
+    int16_t values[CMD_SIZE/2];
+} cmd_data, buf_data;
 
 int
 main(int argc, char *argv[])
 {
     int              sfd, s;
     char             buf[BUF_SIZE];
+    union CMD_DATA buf_data;
     size_t           len;
     ssize_t          nread;
     struct addrinfo  hints;
     struct addrinfo  *result, *rp;
    
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s host port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -59,30 +67,39 @@ main(int argc, char *argv[])
         fprintf(stderr, "Could not connect\n");
         exit(EXIT_FAILURE);
     }
-    /* Send remaining command-line arguments as separate
-       datagrams, and read responses from server */    
-       for (size_t j = 3; j < argc; j++) {
-        len = strlen(argv[j]) + 1;
-                /* +1 for terminating null byte */
 
-        if (len > BUF_SIZE) {
-            fprintf(stderr,
-                    "Ignoring long message in argument %zu\n", j);
-            continue;
-        }
+    // populate the buffer with random values
+    for(int i = 0; i < CMD_SIZE/2; i++){
+        int16_t value = (rand() % 0xFFFF)>>8; // generate small random value
+        cmd_data.values[i] = value;
+        buf_data.values[i] = htons(value);
+    }
 
-        if (write(sfd, argv[j], len) != len) {
-            fprintf(stderr, "partial/failed write\n");
-            exit(EXIT_FAILURE);
-        }
+    /* print the values*/
+    for(int i = 0; i < CMD_SIZE/2; i++){
+        printf("Value %d: %d\n", i, cmd_data.values[i]);
+    }
 
-        nread = read(sfd, buf, BUF_SIZE);
-        if (nread == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
+    /* send command buffer values */
+    len = CMD_SIZE;
 
-        printf("Received %zd bytes: %s\n", nread, buf);
+    if (write(sfd, buf_data.bytes, len) != len) {
+        fprintf(stderr, "partial/failed write\n");
+        exit(EXIT_FAILURE);
+    }
+
+    nread = read(sfd, buf_data.bytes, CMD_SIZE);
+    if (nread == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    /* convert buffer data to host byte order */
+
+    printf("Received %zd bytes\n", nread);
+    for (size_t i = 0; i < nread/2; i++) {
+        buf_data.values[i] = ntohs(buf_data.values[i]);
+        printf("Received value %zu: %d\n", i, buf_data.values[i]);
     }
 
     exit(EXIT_SUCCESS);
